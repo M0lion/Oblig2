4086d44af42c35dc4c204b1153e1cb599e9e5c12
M0lion <bjorn-moholt@hotmail.com>
2014-10-16 15:43:43 +0200

    finished

--- src/Graph.java
+++ src/Graph.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Stack;
 
 
 public class Graph 
@@ -9,6 +10,8 @@ public class Graph
 	private Edge [][]Edges;
 	
 	int countTasks;
+	
+	static int finished = 0;
 	
 	public Graph(String file)
 	{	
@@ -51,6 +54,84 @@ public class Graph
 				}
 			}
 		}
+		
+		if(!isGraphCyclic())
+		{
+			for(Task task : Tasks)
+				if(task.earliestStart == -1)
+					task.calcEarliestStartTime();
+			
+			for(Task task : Tasks)
+				task.calcLatestStartTime();
+		}
+	}
+
+	//simulates doing the tasks and prints result
+	public void start()
+	{
+		int time = 0;
+		int nextUpdate = 0;
+		int tasksToComplete = countTasks;
+		int manpower = 0;
+		int[] runningtasks = new int[Tasks.length]; //0: idle, 1: running, 2: finished
+
+		Stack<Task> started = new Stack<Task>();
+		Stack<Task> finished = new Stack<Task>();
+		
+		while(tasksToComplete > 0)
+		{
+			nextUpdate = Integer.MAX_VALUE;
+			
+			for(int i = 0; i < countTasks; i++)
+			{
+				if(runningtasks[i] == 0 && Tasks[i].earliestStart <= time)
+				{
+					runningtasks[i] = 1;
+					started.add(Tasks[i]);
+					manpower += Tasks[i].staff;
+				}
+				if(runningtasks[i] == 1 && Tasks[i].earliestStart + Tasks[i].time <= time)
+				{
+					runningtasks[i] = 2;
+					finished.add(Tasks[i]);
+					tasksToComplete--;
+					manpower -= Tasks[i].staff;
+				}
+				
+			}
+			
+			if(!started.empty() || !finished.empty())
+			{
+				System.out.println("Time: " + time + "\tCurrent staff: " + manpower);
+
+				while(!started.empty())
+					System.out.println("\tStarted: " + started.pop().id);
+				while(!finished.empty())
+					System.out.println("\tFinished: " + finished.pop().id);
+			}
+			
+			time++;
+		}
+		
+		System.out.println();
+	}
+	
+	public void printCritcals()
+	{
+		for(int i = 0; i < countTasks; i++)
+		{
+			if(Tasks[i].earliestStart == Tasks[i].latestStart)
+				Tasks[i].print();
+		}
+	}
+	
+	//checks if graph is cyclic, if it is prints out the nodes in the found cycle
+	private boolean isGraphCyclic()
+	{
+		if(Tasks[0].checkForLoop(true) == 0)
+			if(Tasks[0].checkForLoop(true) == 0)
+				return false;
+		return true;
 	}
 	
 	public Task getTask(int task) {		
@@ -80,11 +161,16 @@ public class Graph
 	{
 		int id , time , staff ;
 		String name ;
-		int earliestStart , latestStart;
+		int earliestStart = -1;
+		int latestStart = -1;
+		int slack = -1;
 		int cntOutEdges = 0;
 		Edge []outEdges;
 		Edge []inEdges;
 		int cntPredecessors ;
+		
+		//status for finding loops 0:not visited, 1: in progress, 2: found loop
+		int status = 0;
 		
 		public Task(){};
 		
@@ -93,21 +179,145 @@ public class Graph
 			Initialize(input, graph);
 		}
 		
+		private void print()
+		{
+			System.out.println("ID: " + id + "\nName: " + name + "\nTime: " +  time + "\nStaff: " + "\nSlack: 0\nLatest start: " + latestStart);
+			System.out.print("Tasks depending on this:");
+			
+			for(int i = 0; i < outEdges.length; i++)
+			{
+				System.out.print(" " + outEdges[i].to);
+			}
+			System.out.println();
+		}
+		
+		private void calcEarliestStartTime()
+		{
+			earliestStart = 0;
+			
+			for(Edge edge : inEdges)
+			{
+				if(edge != null)
+				{
+					if(edge.fromTask.earliestStart == -1)
+						edge.fromTask.calcEarliestStartTime();
+					
+					int predecessorFinished = edge.fromTask.earliestStart + edge.fromTask.time;
+					
+					if(predecessorFinished > earliestStart)
+						earliestStart = predecessorFinished;
+				}
+			}
+			
+			if(finished < earliestStart + time)
+				finished = earliestStart + time;
+		}
+
+		private void calcLatestStartTime()
+		{
+			latestStart = earliestStart;
+			
+			for(Edge edge : outEdges)
+			{				
+				int nextStart = edge.toTask.earliestStart;
+				
+				if(nextStart > latestStart + time)
+					latestStart = nextStart - time;
+			}
+			
+			if(outEdges.length == 0)
+			{
+				latestStart = finished - time;
+			}
+			
+			slack = latestStart - earliestStart;
+		}
+		
+		//recursively checks for loops in given direction(true with edges, false against edges), returns 0 for no loop, 1 for loop
+		private int checkForLoop(boolean direction)
+		{
+			switch(status)
+			{
+				case 0:
+					status = 1;
+					
+					Task[] tasks;
+					
+					if(direction)
+						tasks = getOutTasks();
+					else
+						tasks = getInTasks();
+					
+					for(int i = 0; i < tasks.length; i++)
+					{
+						int result = tasks[i].checkForLoop(direction);
+						
+						if(result == 2)
+							System.out.println(id);
+						
+						if(result == 2 || result == 1)
+						{						
+							if(status == 2)
+								result = 1;
+							
+							status = 0;
+							return result;
+						}
+					}
+					
+					break;
+				case 1:
+					status = 2;
+					System.out.println("Loop found in in theese tasks:");
+					return 2;
+			}
+			
+			status = 0;
+			return 0;
+		}
+		
+		private Task [] getOutTasks()
+		{
+			Task[] tasks = new Task[outEdges.length];
+			
+			for(int i = 0; i < tasks.length; i++)
+			{
+				tasks[i] = outEdges[i].toTask;
+			}
+			
+			return tasks;
+		}
+		
+		private Task [] getInTasks()
+		{
+			Task[] tasks = new Task[outEdges.length];
+			
+			for(int i = 0; i < tasks.length; i++)
+			{
+				tasks[i] = inEdges[i].fromTask;
+			}
+			
+			return tasks;
+		}
+		
 		public void Initialize(String input, Graph graph)
 		{
-			String []lines = input.split("\\s+");
+			String []tokens = input.split("\\s+");
 			
-			id = Integer.parseInt(lines[0]); 
-			name = lines[1];
-			time = Integer.parseInt(lines[2]);
-			staff = Integer.parseInt(lines[3]);
+			id = Integer.parseInt(tokens[0]); 
+			name = tokens[1];
+			time = Integer.parseInt(tokens[2]);
+			staff = Integer.parseInt(tokens[3]);
 			
-			inEdges = new Edge[lines.length - 4];
+			inEdges = new Edge[tokens.length - 4];
 			int edge = 0;
-			for(int i = 4; i < lines.length; i++)
+			for(int i = 4; i < tokens.length; i++)
 			{
-				if(Integer.parseInt(lines[i]) != 0)
-					addInEdge(Integer.parseInt(lines[i]), edge, graph);
+				if(Integer.parseInt(tokens[i]) != 0)
+				{
+					addInEdge(Integer.parseInt(tokens[i]), edge, graph);
+					edge++;
+				}
 			}	
 		}
 		
@@ -147,7 +357,7 @@ public class Graph
 				System.out.print("\t");
 			
 			System.out.println((id) + name);
-			int a = Edges.length;
+			
 			for(int i = 0; i < outEdges.length; i++)
 				outEdges[i].toTask.print(t + 1);
 		}
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String []args)
 	{
 		Graph graph = new Graph("buildhouse1.txt");
-		
-		graph.getTask(5).print(0);
+		graph.start();
+		graph.printCritcals();
 	}
 }
